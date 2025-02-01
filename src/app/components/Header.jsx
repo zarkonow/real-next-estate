454efc89f3bd2f6fd67c91d7e1323bee72a7c4b2
zarkonow <stojanov1987@gmail.com>
2025-02-01 21:40:33 +0100

    add react-icons dependency and enhance Header component with search functionality and navigation links

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import React from 'react'
+import { FaSearch } from 'react-icons/fa'
 
 
 export default function Header() {
@@ -8,10 +9,27 @@ export default function Header() {
         <div className='container mx-auto flex justify-between items-center'>
         <Link href='/'>
         <h1 className='text-2xl font-bold cursor-pointer'>
-            <span className=''>Real</span>
-            <span className=''>Estate</span>
+            <span className='text-slate-500'>Real</span>
+            <span className='text-slate-700'>Estate</span>
         </h1>
         </Link>
+        <form className='flex items-center'>
+            <input type="text" placeholder="Search" className='p-2 rounded-lg'/>
+            <button>
+                <FaSearch className='text-slate-600' />
+                Search</button>
+        </form>
+        <ul>
+            <li className='inline-block mx-2'>
+                <Link href='/'>Home</Link>
+            </li>
+            <li className='inline-block mx-2'>
+                <Link href='/about'>About</Link>
+            </li>
+            <li className='inline-block mx-2'>
+                <Link href='/contact'>Contact</Link>
+            </li>
+        </ul>
         </div>
         </header>
   )
