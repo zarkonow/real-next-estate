4a78cb1a2a0a60bcf2742e2f3d178c2caa373b03
zarkonow <stojanov1987@gmail.com>
2025-02-02 01:04:55 +0100

    add About page with company information and mission statement

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header/>
+        
         {children}
       </body>
     </html>
