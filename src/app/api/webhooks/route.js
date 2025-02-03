213de87c81249aa7950c06a2567960397e58783b
zarkonow <stojanov1987@gmail.com>
2025-02-03 20:17:11 +0100

    fix: update clerkClient import path in webhook route

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -1,7 +1,8 @@
 import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { createOrUpdateUser, deleteUser } from "@/lib/actions/user";
-import { clerkClient } from "@clerk/nextjs/server";
+import { clerkClient } from "@clerk/nextjs/dist/types/server";
+
 
 export async function POST(req) {
   const SIGNING_SECRET = process.env.SIGNING_SECRET;
