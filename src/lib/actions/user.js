7353912c861f34ad966639b483b29c013ee5e18b
zarkonow <stojanov1987@gmail.com>
2025-02-03 17:21:55 +0100

    refactor user actions; improve error logging and update user model fields

--- src/lib/actions/user.js
+++ src/lib/actions/user.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model";
+
 import { connect } from "../mongodb/mongoose";
 
 export const createOrUpdateUser = async (
@@ -16,24 +17,23 @@ export const createOrUpdateUser = async (
         $set: {
           firstName: first_name,
           lastName: last_name,
-          imageUrl: image_url,
-          email: email_addresses[0].email_addresse,
+          profilePicture: image_url,
+          email: email_addresses[0].email_address,
         },
       },
       { upsert: true, new: true }
     );
     return user;
   } catch (error) {
-    console.log("Error Update or Create User", error);
+    console.log("Error: Could not create or update user:", error);
   }
 };
 
-export const deleteUser = async (id)=>{
-    try {
-        await connect();
-        await User.findOneAndUpdate({clerkId: id})
-    } catch (error) {
-        console.log("Error Deleting User", error);
-        
-    }
- }
+export const deleteUser = async (id) => {
+  try {
+    await connect();
+    await User.findOneAndDelete({ clerkId: id });
+  } catch (error) {
+    console.log("Error: Could not delete user:", error);
+  }
+};
