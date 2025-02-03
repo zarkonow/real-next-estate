8535f1aa5ef81fa2b0e4ed2e9da613b8f1ab7e08
zarkonow <stojanov1987@gmail.com>
2025-02-04 00:03:34 +0100

    fix: update baseUrl in jsconfig.json and correct import path for user actions in webhook route

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -1,6 +1,6 @@
 import { Webhook } from 'svix';
 import { headers } from 'next/headers';
-import { createOrUpdateUser, deleteUser } from '@/lib/actions/user';
+import { createOrUpdateUser, deleteUser } from '@/lib/actions/users';
 import { clerkClient } from '@clerk/nextjs/server';
 
 export async function POST(req) {
